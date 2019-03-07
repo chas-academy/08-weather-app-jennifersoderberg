cef7012cb744869ad1430572e2c085d065029cef
Jennifer Soederberg <jennifer.soderberg@chasacademy.se>
2019-03-07 16:12:02 +0100

    Add components: Header, Main, Footer, TodaysForecast, WeeklyForecast

---
+++ src/components/TodaysForecast.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TodaysForecast() {
+    return(
+        <div>
+            <h1>Todays forecast!</h1>
+        </div>
+    )
+}
---
+++ src/components/WeeklyForecast.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function WeeklyForecast() {
+    return(
+        <div>
+            <h1>Weekly forecast!</h1>
+        </div>
+    )
+}
